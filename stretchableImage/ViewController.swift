ef3b168d8ac12e0e1d201bce9cc2558222bedf9e
tommy <tommy5566123@gmail.com>
2022-01-20 15:23:17 +0800

    Initial Commit

---
+++ stretchableImage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  stretchableImage
+//
+//  Created by 蔡尚諺 on 2022/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
